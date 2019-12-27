70cd2126b461258b62b853a058526241ba42e6a7
John Zechlin <john@phxdevs.com>
2019-12-26 23:16:22 -0700

    nginx config updates to make things actually work

--- vagrant_provision/modules.sh
+++ vagrant_provision/modules.sh
@@ -14,7 +14,10 @@ systemctl enable hello_world
 # this is just a "fun" way to ensure that we add the config the first time, but also don't 
 # add it more than once
 sed -i '/include\ \/var\/www\/flask-modules\/nginx_confs\/\*.conf;/d' /etc/nginx/nginx.conf
-sed -i '/\/etc\/nginx\/conf.d\/\*.conf/a\ \ \ \ include /var/www/flask-modules/nginx_confs/*.conf;' /etc/nginx/nginx.conf
+sed -i '/\/etc\/nginx\/default.d\/\*.conf/a\ \ \ \ \ \ \ \ include /var/www/flask-modules/nginx_confs/*.conf;' /etc/nginx/nginx.conf
+sed -ri '/location \//,/.*\}/d' /etc/nginx/nginx.conf
+
+
 
 systemctl start nginx
 systemctl enable nginx
